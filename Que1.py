dba88d48c14523ed67d2bbeb63ba32bd444fe0b4
poonamw123 <poonamwategaonkar@gmail.com>
2026-06-15 21:36:22 +0530

    Initial commit

---
+++ Que1.py
@@ -0,0 +1,4 @@
+x = 10
+print("Value of x : ", x)
+print("Type of x : ", type(x))
+print("Memory Address of x : ", id(x))
---
+++ Que6.py
@@ -0,0 +1,30 @@
+# Addition
+
+print("Enter first number : ")
+No1 = int(input())
+
+print("Enter second number : ")
+No2 = int(input())
+
+Ans = No1 + No2
+
+print("Addition is : ",Ans)
+
+
+#Substraction
+
+Ans = No1 - No2
+
+print("Substraction is : ",Ans)
+
+#Multiplication
+
+Ans = No1 * No2
+
+print("Multiplication is : ",Ans)
+
+#Division
+
+Ans = No1 / No2
+
+print("Division is : ",Ans)
